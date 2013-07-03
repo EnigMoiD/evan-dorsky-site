87e7e09b6cd8466be6c25fd7f38ef6dc6fa0c140
Evan Dorsky <evan.dorsky@icloud.com>
2013-07-02 23:17:34 -0400

    Removed bourbon, that was the wrong bourbon.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
+// Using jade for templates
 app.engine('jade', require('jade').__express);
+
+// Using sass for stylesheets
+var sass = require('node-sass');
+
 
 app.get('/', function(req, res){
 	res.send('hello world!');
