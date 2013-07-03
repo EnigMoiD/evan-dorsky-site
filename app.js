0ce3a3f3ef8d4e5853cf450e8b50fc2c5a748420
Evan Dorsky <evan.dorsky@icloud.com>
2013-07-03 00:43:50 -0400

    Working templates, etc. Broken stylesheets.

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require('express');
+// Using express for webapp
+var express = require('express'),
+// Using sass for stylesheets
+sass = require('node-sass');
+
 var app = express();
 
-// Using jade for templates
-app.engine('jade', require('jade').__express);
+// Using jade for backend templates
+app.set('view engine', 'jade');
 
-// Using sass for stylesheets
-var sass = require('node-sass');
+// Setting up directories
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/static'));
 
-
-app.get('/', function(req, res){
-	res.send('hello world!');
-});
-
-app.get('/test', function(req, res){
-	res.send('You went to test!');
-});
+app.get('/', function (req, res) {
+	res.render('index',
+		{ title : 'Home' }
+		)
+})
 
 app.listen(3000);
