8fd18dd67e3a37939773dbf28f35ae5e1a305695
Evan Dorsky <evan.dorsky@icloud.com>
2013-07-03 00:54:43 -0400

    Replaced sass with stylus+nib, working.

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 // Using express for webapp
 var express = require('express'),
-// Using sass for stylesheets
-sass = require('node-sass');
+// Using stylus for stylesheets
+stylus = require('stylus'),
+// And nib like bourbon
+nib = require('nib');
 
+// Instantiating app
 var app = express();
 
-// Using jade for backend templates
-app.set('view engine', 'jade');
+// Setting up app
+app.use(express.logger('dev'));
 
 // Setting up directories
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/static'));
+
+// Using jade for backend templates
+app.set('view engine', 'jade');
+
+// Setting up stylus
+function compile(str, path) {
+	return stylus(str)
+		.set('filename', path)
+		.use(nib());
+}
+app.use(stylus.middleware(
+{
+	src: __dirname + '/static', 
+	compile: compile
+}
+)
+);
 
 app.get('/', function (req, res) {
 	res.render('index',
