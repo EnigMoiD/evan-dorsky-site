f95412f65c0b73d056c636892c5f52a53474c580
Evan Dorsky <evan.dorsky@icloud.com>
2013-07-07 02:32:18 -0400

    Renamed static->public and post template working

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+var
 // Using express for webapp
-var express = require('express'),
+express = require('express'),
+// And the express webapp
+app = express(),
 // Using stylus for stylesheets
 stylus = require('stylus'),
 // And nib like bourbon
-nib = require('nib');
+nib = require('nib'),
+// Poet for blog
+Poet = require('poet');
 
-// Instantiating app
-var app = express();
+var poet = Poet(app, {
+	posts: __dirname + '/posts',
+	postsPerPage: 5,
+	metaFormat: 'json'
+});
 
-// Setting up app
-app.use(express.logger('dev'));
+poet.init(function() {
+	// Nothing yet!
+});
 
 // Setting up directories
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/public'));
 
 // Using jade for backend templates
 app.set('view engine', 'jade');
@@ -24,18 +33,20 @@ function compile(str, path) {
 		.set('filename', path)
 		.use(nib());
 }
-app.use(stylus.middleware(
-{
-	src: __dirname + '/static', 
-	compile: compile
-}
-)
-);
+// app.use(stylus.middleware(
+// {
+// 	src: __dirname + '/public', 
+// 	compile: compile
+// }
+// )
+// );
+
+app.use(app.router);
 
 app.get('/', function (req, res) {
 	res.render('index',
 		{ title : 'Home' }
 		)
-})
+});
 
 app.listen(3000);
---
+++ posts/test.md
@@ -0,0 +1,8 @@
+{{{
+  "title": "test",
+  "tags": ["blog", "fun"],
+  "category": "javascript",
+  "date": "7-5-2013"
+}}}
+
+It is content...
