25a753d66cd2dcd0477828542ed5a4cd8b51aabe
Evan Dorsky <evan.dorsky@icloud.com>
2013-07-18 22:41:44 -0400

    jQuery for topbar list item widths

---
+++ public/js/layout.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	setupTopBar();
+});
+
+function setupTopBar () {
+	var topBarItems = $('.topbar ul li');
+	var itemWidth = 100 / topBarItems.length;
+
+	topBarItems.width(itemWidth+'%');
+}
